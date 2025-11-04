068d30ad30bcfe705c159684d36af59ff9196a4b
pablotq <pablo.cstds@gmail.com>
2025-11-04 17:23:59 -0300

    adicionado funcionalidade de esconder o botão "Ver Projeto" caso não tenha um link

--- src/js/modal-projetos.js
+++ src/js/modal-projetos.js
@@ -6,7 +6,7 @@ const projetos = {
     link: "",
     tecnologias: ["Python", "Discord.py"],
     detalhes: "Este bot foi desenvolvido para auxiliar mestres e jogadores do RPG Pokerole, automatizando a geração de pokémons e facilitando o gerenciamento das fichas.",
-    github:"https://github.com/pablotq/PokeRole-Discord.py/tree/main"
+    github: "https://github.com/pablotq/PokeRole-Discord.py/tree/main"
   },
   botflix: {
     titulo: "BotFlix",
@@ -15,7 +15,7 @@ const projetos = {
     link: "https://pablotq.github.io/botflix",
     tecnologias: ["HTML", "CSS", "JavaScript(ES6)", "n8n", "API TMDB"],
     detalhes: "Este bot foi desenvolvido para indicar uma sugestão de filme ou série de acorodo com os detalhes que são passados para ele",
-    github:"https://github.com/pablotq/botflix"
+    github: "https://github.com/pablotq/botflix"
   },
   visualisadorPerfilGithub: {
     titulo: "Visualisador de Perfil do Github",
@@ -24,7 +24,7 @@ const projetos = {
     link: "https://pablotq.github.io/visualizador-perfil-github",
     tecnologias: ["HTML", "CSS", "JavaScript(ES6)", "API Github"],
     detalhes: "Esse projeto foi desenvolvido com o intuito de buscar informações do usuário exibindo seu avatar, nome e biografia. Além de listar os 10 últimos repositórios criados pelo usuário, com links diretos.",
-    github:"https://github.com/pablotq/visualizador-perfil-github"
+    github: "https://github.com/pablotq/visualizador-perfil-github"
   },
   dbz: {
     titulo: "Projeto DBZ",
@@ -33,7 +33,7 @@ const projetos = {
     link: "https://pablotq.github.io/projeto-dbz",
     tecnologias: ["HTML", "CSS", "JavaScript(ES6)"],
     detalhes: "Esse projeto foi desenvolvido no início do meu curso com objetivo de aprender responsividade.",
-    github:"https://github.com/pablotq/projeto-dbz"
+    github: "https://github.com/pablotq/projeto-dbz"
   },
   // Adicione mais projetos aqui
 };
@@ -42,14 +42,14 @@ document.querySelectorAll('.projeto').forEach(projeto => {
   projeto.addEventListener('click', () => {
     const projetoId = projeto.dataset.projeto;
     const projetoData = projetos[projetoId];
-    
+
     const modal = document.getElementById('modal');
     const modalTitle = modal.querySelector('h2');
     const modalImg = modal.querySelector('img');
     const modalInfo = modal.querySelector('.modal-info');
     const modalLink = document.getElementById('modal-link');
     const modalGithub = document.getElementById('modal-github');
-    
+
     modalTitle.textContent = projetoData.titulo;
     modalImg.src = projetoData.imagem;
     modalImg.alt = projetoData.titulo;
@@ -58,9 +58,14 @@ document.querySelectorAll('.projeto').forEach(projeto => {
       <p><strong>Tecnologias:</strong> ${projetoData.tecnologias.join(', ')}</p>
       <p>${projetoData.detalhes}</p>
     `;
-    modalLink.href = projetoData.link;
+    if (projetoData.link === "") {
+      modalLink.classList.add('hidden'); 
+    } else {
+      modalLink.classList.remove('hidden');
+      modalLink.href = projetoData.link;
+    }
     modalGithub.href = projetoData.github
-    
+
     modal.style.display = "block";
   });
 });
